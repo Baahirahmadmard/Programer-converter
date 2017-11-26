ff7e924a8205b283e4d138bacc795d8ee61c5715
Bashirahmadmard <bashirahmadmard@gmail.com>
2017-11-26 11:35:48 +0430

    main file update

--- programer_converter.java
+++ programer_converter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Numbers {
-	int binarynum, octalnum, decimalnum, hexadecimalnum;
+	static int binarynum, octalnum, decimalnum, hexadecimalnum;
 	Scanner input=new Scanner(System.in);
 
 	public void geter(){
@@ -39,10 +39,47 @@ class Numbers {
 	}
 }
 
+class Decimal extends Numbers{
+	Numbers d=new Numbers();
+	int binary, octal, decimal=d.decimalnum, hexadecimal, i=0, i2=1, base;
+	int[] rm=new int[99];
+
+	public void convert_decimal(){
+		while(i2<5){
+			if(i2==1)
+				base=2;
+			else if (i2==2)
+				base=8;
+			else if (i2==3)
+				base=10;
+			else if (i2==4)
+				base=16;
+
+			while(decimal!=0){
+		
+				rm[i]=decimal%base;
+				decimal=decimal/base;
+				i++;
+			}
+		i2++;
+		}
+	}
+
+	public void seter(){
+		System.out.println("Your output is ");
+		for (int i=0; i<=this.i; i++) {
+			System.out.print(rm[i]);
+		}
+	}
+}
+
 class programer_converter {
 	public static void main(String[] args) {
 		Numbers num=new Numbers();
 		num.geter();
+		Decimal dnum=new Decimal();
+		dnum.convert_decimal();
+		dnum.seter();
 	}
 }
 //the end
